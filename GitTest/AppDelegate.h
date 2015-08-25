11184b38e0aa6393d7d46410e89768cfd322ecdb
WenFeng Zhang <wenfeng0218@icloud.com>
2015-08-25 16:31:11 +0800

    Create a new Xcode project (for iPhone, using Objective-C)

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by 张文峰 on 15/8/25.
+//  Copyright © 2015年 皓月君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by 张文峰 on 15/8/25.
+//  Copyright © 2015年 皓月君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
